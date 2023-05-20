23eda894ebabd8ee70c67521b40de49f9d0d9326
Rishav Nath Pati <patirishavnath@gmail.com>
2023-05-20 11:56:50 +0530

    Time based sorting

--- main.py
+++ main.py
@@ -8,10 +8,35 @@ cursor = connection.cursor()
 MAX_QUESTIONS = 5
 # Retrieve the quiz data from the database
 quiz_data = cursor.execute(
-    "SELECT serial_number,question, option1, option2, option3, option4, correct_answer, time, priority FROM quiz_table"
+    "SELECT serial_number, question, option1, option2, option3, option4, correct_answer, time, priority FROM quiz_table ORDER BY time ASC LIMIT 10"
 ).fetchall()
 
+for question_data in quiz_data:
+    (
+        serial_number,
+        question,
+        correct_answer,
+        option1,
+        option2,
+        option3,
+        option4,
+        time,
+        priority,
+    ) = question_data
+    print(f"Serial Number: {serial_number}")
+    print(f"Question: {question}")
+    print(f"Options:")
+    # print(f"1. {option1}")
+    # print(f"2. {option2}")
+    # print(f"3. {option3}")
+    # print(f"4. {option4}")
+    print(f"Correct Answer: {correct_answer}")
+    print(f"Time: {time}")
+    print(f"Priority: {priority}")
+    print("-----------------------")
+
 random_questions = random.sample(quiz_data, 5)
+# print(random_questions)
 
 
 # Implement the spaced repetition algorithm
@@ -32,7 +57,7 @@ for question_data in random_questions:
     # score = score_dict.get(question, 0)  # retrieve the score for the question
 
     # Present the question to the user and collect their response
-    print(question)
+    print(serial_number, " ", question)
     print("Options:")
     print(f"1. {option1}")
     print(f"2. {option2}")
