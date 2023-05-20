03d5d406c85502410bd3b0d1c16b3c9ffdb0394b
BhartiSingh9 <bharti.singhbs9681@gmail.com>
2023-05-20 11:22:42 +0530

    question updated

--- sql.sql
+++ sql.sql
@@ -24,4 +24,87 @@
 
 -- -- Continue inserting the remaining 6 questions in a similar manner
 
+<<<<<<< HEAD
 -- SELECT * FROM quiz_table;
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (1, 'What is the capital of France?', 'Paris', 'London', 'Berlin', 'Madrid', 'Paris', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (5, 'Which planet is known as the Red Planet?', 'Mars', 'Venus', 'Jupiter', 'Saturn', 'Mars', 0);
+
+-- SELECT * FROM quiz_table;
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (6, 'What is the capital city of France?', 'London', 'Rome', 'Paris', 'Berlin', 'Paris', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (7, 'Who painted the Mona Lisa?', 'Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo', 'Leonardo da Vinci', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (8, 'What is the chemical symbol for gold?', 'Au', 'Ag', 'Fe', 'Hg', 'Au', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (9, 'Who wrote the play "Romeo and Juliet"?', 'William Shakespeare', 'George Bernard Shaw', 'Oscar Wilde', 'Jane Austen', 'William Shakespeare', 0);
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (10, 'Who is the author of the play "Hamlet"?', 'William Shakespeare', 'Arthur Miller', 'George Bernard Shaw', 'Tennessee Williams', 'William Shakespeare', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (11, 'What is the capital city of Brazil?', 'Rio de Janeiro', 'São Paulo', 'Brasília', 'Buenos Aires', 'Brasília', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (12, 'What is the chemical symbol for iron?', 'Fe', 'Au', 'Ag', 'Pb', 'Fe', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (13, 'Who wrote the novel "To Kill a Mockingbird"?', 'Harper Lee', 'J.D. Salinger', 'F. Scott Fitzgerald', 'Mark Twain', 'Harper Lee', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (14, 'Which country is known as the "Land of the Midnight Sun"?', 'Norway', 'Sweden', 'Finland', 'Denmark', 'Norway', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (15, 'What is the chemical symbol for potassium?', 'K', 'Na', 'Ca', 'Mg', 'K', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (16, 'Who painted the famous artwork "The Scream"?', 'Edvard Munch', 'Pablo Picasso', 'Claude Monet', 'Salvador Dalí', 'Edvard Munch', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (17, 'Which country is home to the Taj Mahal?', 'India', 'China', 'Japan', 'Thailand', 'India', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (18, 'Who is the author of the novel "The Great Gatsby"?', 'F. Scott Fitzgerald', 'Ernest Hemingway', 'Virginia Woolf', 'George Orwell', 'F. Scott Fitzgerald', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (19, 'What is the largest internal organ in the human body?', 'Liver', 'Heart', 'Lungs', 'Stomach', 'Liver', 0);
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (20, 'What is the largest country in the world by land area?', 'Russia', 'China', 'United States', 'Canada', 'Russia', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (21, 'Who painted the famous artwork "The Persistence of Memory"?', 'Salvador Dalí', 'Vincent van Gogh', 'Pablo Picasso', 'Leonardo da Vinci', 'Salvador Dalí', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (22, 'What is the chemical symbol for silver?', 'Ag', 'Au', 'Hg', 'Fe', 'Ag', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (23, 'Who wrote the novel "1984"?', 'George Orwell', 'Aldous Huxley', 'Ray Bradbury', 'George R.R. Martin', 'George Orwell', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (24, 'Which city is known as the "Eternal City"?', 'Rome', 'Athens', 'Cairo', 'Istanbul', 'Rome', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (25, 'What is the chemical symbol for calcium?', 'Ca', 'Na', 'K', 'Mg', 'Ca', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (26, 'Who painted the famous artwork "The Girl with a Pearl Earring"?', 'Johannes Vermeer', 'Claude Monet', 'Edgar Degas', 'Auguste Renoir', 'Johannes Vermeer', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (27, 'Which country is home to the Pyramids of Giza?', 'Egypt', 'Mexico', 'Greece', 'Peru', 'Egypt', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (28, 'Who is the author of the novel "To Kill a Mockingbird"?', 'Harper Lee', 'J.D. Salinger', 'F. Scott Fitzgerald', 'Mark Twain', 'Harper Lee', 0);
+
+-- INSERT INTO quiz_table (serial_number, question, option1, option2, option3, option4, correct_answer, priority)
+-- VALUES (29, 'What is the largest bone in the human body?', 'Femur', 'Humerus', 'Tibia', 'Radius', 'Femur', 0);
+-- SELECT * FROM quiz_table;
+
+=======
+SELECT * FROM quiz_table;
+>>>>>>> d6fa0cf49191869ee7cda0fd498d5213a069d0cd
