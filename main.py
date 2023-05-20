975a8d048bb1333415d6679e7a2f124387bdf7c8
Rishav Nath Pati <patirishavnath@gmail.com>
2023-05-20 12:28:04 +0530

    Active recall algo

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
 import sqlite3
 import datetime
 import random
+import os
+import platform
+
+MAX_QUESTIONS = 5
+
+def clear_terminal():
+    # Check the operating system
+    system = platform.system()
+
+    # Clear the terminal screen based on the operating system
+    if system == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+clear_terminal()
+
 
 # Connect to the SQLite database
 connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
-MAX_QUESTIONS = 5
-# Retrieve the quiz data from the database
-quiz_data = cursor.execute(
-    "SELECT serial_number, question, option1, option2, option3, option4, correct_answer, time, priority FROM quiz_table ORDER BY time ASC LIMIT 10"
+
+
+# Retrieving the quiz data from the database
+
+time_questions = cursor.execute(
+    "SELECT serial_number, question, option1, option2, option3, option4, correct_answer, time, priority FROM quiz_table ORDER BY time ASC LIMIT 13"
 ).fetchall()
 
-for question_data in quiz_data:
+priority_questions = cursor.execute(
+    "SELECT serial_number, question, option1, option2, option3, option4, correct_answer, time, priority FROM quiz_table ORDER BY priority DESC LIMIT 12"
+).fetchall()
+
+quiz_question = ()
+# Set of 10 questions based on intersection
+intersection_questions = set(time_questions) & set(priority_questions)
+
+# Select the first 5 questions from the intersection
+intersection_questions = list(intersection_questions)[:5]
+
+# Set of 3 questions based on oldest time, Sort by time and select the first 3 questions
+oldest_time_questions = sorted(time_questions, key=lambda x: x[7])[:3]
+
+# Set of 2 questions based on highest priority, Sort by priority and select the first 2 questions
+highest_priority_questions = sorted(
+    priority_questions, key=lambda x: x[8], reverse=True
+)[:2]
+
+# Combine the question sets
+quiz_question = (
+    intersection_questions + oldest_time_questions + highest_priority_questions
+)
+
+
+for question_data in quiz_question:
     (
         serial_number,
         question,
@@ -35,11 +79,7 @@ for question_data in quiz_data:
     print(f"Priority: {priority}")
     print("-----------------------")
 
-random_questions = random.sample(quiz_data, 5)
-# print(random_questions)
-
-
-# Implement the spaced repetition algorithm
+random_questions = random.sample(quiz_question, 5)
 
 for question_data in random_questions:
     (
@@ -76,8 +116,11 @@ for question_data in random_questions:
     # Check if the user's answer is correct
     if user_answer == correct_answer:
         print("Correct answer!")
-        # Increment the score if the answer is correct
-
+        # set priority to 0 once the answer is correct
+        cursor.execute(
+            "UPDATE quiz_table SET priority = ? WHERE serial_number = ?",
+            (0, serial_number),
+        )
     else:
         print(f"Incorrect! The correct answer is {correct_answer}.")
         cursor.execute(
